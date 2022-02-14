9de36247b222736fabd1af0ea49ab548a05dff30
parajuliamit <be2017se587@gces.edu.np>
2022-02-14 13:10:33 +0545

    changes

--- grocery_nepal/lib/modules/order_details/order_details_screen.dart
+++ grocery_nepal/lib/modules/order_details/order_details_screen.dart
@@ -57,7 +57,7 @@ class OrderDetailsScreen extends StatelessWidget {
                                     style: TextStyle(
                                         color: controller.orderDetail!.status ==
                                                 'Delivered'
-                                            ? greenColor
+                                            ? Colors.black
                                             : controller.orderDetail!.status ==
                                                     'Cancelled'
                                                 ? Colors.red
